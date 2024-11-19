076cea22ba55f476786856de799de62928a9dd8e
LiAuTraver <me@liautraver.dev>
2024-11-20 00:45:13 +0800

    complete scanning: add missing  in switch (n/n)

--- src/Token.cpp
+++ src/Token.cpp
@@ -58,122 +58,122 @@ Token::string_t Token::to_string() const {
   /// @note cannot use `string_view_t`, or the string_view will be destroyed.
   ///       eg: `42.0` -> `▯2.0`
   ///                      ^ actually a `\0`, but cannot be displayed.
-  auto literal_str = ""s;
+  auto literal_sv = ""sv;
   if (!literal.has_value())
-    literal_str = "null"s;
+    literal_sv = "null"sv;
   switch (type.type) {
   case kMonostate:
     type_sv = "MONOSTATE"sv;
     lexeme_sv = "null"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kLeftParen:
     type_sv = "LEFT_PAREN"sv;
     lexeme_sv = "("sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kRightParen:
     type_sv = "RIGHT_PAREN"sv;
     lexeme_sv = ")"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kLeftBrace:
     type_sv = "LEFT_BRACE"sv;
     lexeme_sv = "{"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kRightBrace:
     type_sv = "RIGHT_BRACE"sv;
     lexeme_sv = "}"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kComma:
     type_sv = "COMMA"sv;
     lexeme_sv = ","sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kDot:
     type_sv = "DOT"sv;
     lexeme_sv = "."sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kMinus:
     type_sv = "MINUS"sv;
     lexeme_sv = "-"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kPlus:
     type_sv = "PLUS"sv;
     lexeme_sv = "+"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kSemicolon:
     type_sv = "SEMICOLON"sv;
     lexeme_sv = ";"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kSlash:
     type_sv = "SLASH"sv;
     lexeme_sv = "/"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kStar:
     type_sv = "STAR"sv;
     lexeme_sv = "*"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kBang:
     type_sv = "BANG"sv;
     lexeme_sv = "!"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kBangEqual:
     type_sv = "BANG_EQUAL"sv;
     lexeme_sv = "!="sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kEqual:
     type_sv = "EQUAL"sv;
     lexeme_sv = "="sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kEqualEqual:
     type_sv = "EQUAL_EQUAL"sv;
     lexeme_sv = "=="sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kGreater:
     type_sv = "GREATER"sv;
     lexeme_sv = ">"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kGreaterEqual:
     type_sv = "GREATER_EQUAL"sv;
     lexeme_sv = ">="sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kLess:
     type_sv = "LESS"sv;
     lexeme_sv = "<"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kLessEqual:
     type_sv = "LESS_EQUAL"sv;
     lexeme_sv = "<="sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kIdentifier:
     type_sv = "IDENTIFIER"sv;
     lexeme_sv = lexeme;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kString:
     type_sv = "STRING"sv;
     lexeme_sv = lexeme;
     contract_assert(lexeme_sv.front() == '"' && lexeme_sv.back() == '"');
-    literal_str =
+    literal_sv =
         cast_literal<string_view_t>().value_or("<failed to access data>");
-    contract_assert(lexeme_sv.substr(1, lexeme_sv.size() - 2), literal_str);
+    contract_assert(lexeme_sv.substr(1, lexeme_sv.size() - 2), literal_sv);
     break;
   case kNumber:
     type_sv = "NUMBER"sv;
@@ -184,95 +184,97 @@ Token::string_t Token::to_string() const {
               signaling_NaN()); // format *can* format NaN to "nan"
       if (is_integer(num_value))
         // 42 -> 42.0
-        literal_str = string_t{format("{:.1f}", num_value)};
+        return format("{} {} {:.1f}", type_sv.data(), lexeme_sv.data(), num_value);
+        // literal_sv = string_t{format("{:.1f}", num_value)};
       else
         // leave as is
-        literal_str = string_t{format("{}", num_value)};
+        return format("{} {} {}", type_sv.data(), lexeme_sv.data(), num_value);
     }
     break;
   case kAnd:
     type_sv = "AND"sv;
     lexeme_sv = "and"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kClass:
     type_sv = "CLASS"sv;
     lexeme_sv = "class"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kElse:
     type_sv = "ELSE"sv;
     lexeme_sv = "else"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kFalse:
     type_sv = "FALSE"sv;
     lexeme_sv = "false"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kFun:
     type_sv = "fun"sv;
     lexeme_sv = "fun"sv;
-    literal_str = "null"s; // currently no literal for fun // @todo
+    literal_sv = "null"sv; // currently no literal for fun // @todo
     break;
   case kFor:
     type_sv = "FOR"sv;
     lexeme_sv = "for"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kIf:
     type_sv = "IF"sv;
     lexeme_sv = "if"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kNil:
     type_sv = "NIL"sv;
     lexeme_sv = "nil"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kOr:
     type_sv = "OR"sv;
     lexeme_sv = "or"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kPrint:
     type_sv = "PRINT"sv;
     lexeme_sv = "print"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kReturn:
     type_sv = "RETURN"sv;
     lexeme_sv = "return"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kSuper:
     type_sv = "SUPER"sv;
     lexeme_sv = "super"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kThis:
     type_sv = "THIS"sv;
     lexeme_sv = "this"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kTrue:
     type_sv = "TRUE"sv;
     lexeme_sv = "true"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kVar:
     type_sv = "VAR"sv;
     lexeme_sv = "var"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
+    break;
   case kWhile:
     type_sv = "WHILE"sv;
     lexeme_sv = "while"sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kEndOfFile:
     type_sv = "EOF"sv;
     lexeme_sv = ""sv;
-    literal_str = "null"s;
+    literal_sv = "null"sv;
     break;
   case kLexError:
     /// @note message is different from the other cases.
@@ -282,7 +284,6 @@ Token::string_t Token::to_string() const {
   default:
     break;
   }
-  return string_t{type_sv} + " " + string_t{lexeme_sv} + " " +
-         string_t{literal_str};
+  return format("{} {} {}", type_sv.data(), lexeme_sv.data(), literal_sv.data());
 }
 } // namespace net::ancillarycat::loxograph
